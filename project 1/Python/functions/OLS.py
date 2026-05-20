7b88f9869c8309d5730358b0285472b43aa982bc
Gerard Wu <230164507+GerardWuHeston@users.noreply.github.com>
2026-05-20 12:23:32 -0400

    chore: save Codex changes

---
+++ project 1/Python/functions/OLS.py
@@ -0,0 +1,19 @@
+import cvxpy as cp
+import numpy as np
+
+
+def OLS(returns, factRet, lambda_, K):
+    """
+    % Use this function to perform an OLS regression. Note that you will
+    % not use lambda or K in this model (lambda is for LASSO, and K is for
+    % BSS).
+    """
+
+    # *************** WRITE YOUR CODE HERE ***************
+    # ----------------------------------------------------------------------
+
+    # mu =          % n x 1 vector of asset exp. returns
+    # Q  =          % n x n asset covariance matrix
+    # ----------------------------------------------------------------------
+
+    return mu, Q
